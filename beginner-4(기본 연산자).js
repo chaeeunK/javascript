1054c530d47410b870a3ddbf9bfc4e818c245412
김채은 <101077376+chaeeunK@users.noreply.github.com>
2022-05-27 10:09:23 +0900

    기본 연산자
    기본 연산자

---
+++ beginner-4(기본 연산자).js
@@ -0,0 +1,15 @@
+let num = 10;
+//num = num + 5;
+num += 5;
+//num -= 5;
+//num /= 5;
+//num *= 5;
+//num %= 5;
+console.log(num);
+
+//증가 연산자, 감소 연산자
+let NUM = 10;
+//let result = NUM++; (++을 뒤에 넣으면 증가되기 전 값이 들어옴)
+let result = ++NUM;
+
+console.log(result);
