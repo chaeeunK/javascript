c9ca7e47f596c7d31fd92a0049bdf4a2b6eba6ce
김채은 <101077376+chaeeunK@users.noreply.github.com>
2022-05-30 15:53:25 +0900

    비교 연산자, 조건문 (if, else)
    비교 연산자, 조건문 (if, else)

---
+++ beginner-5(if,else).js
@@ -0,0 +1,19 @@
+/*console.log(10>5);//true
+console.log(10 == 5);//false
+console.log(10 != 5);//true
+
+const a = 1;
+const b = "1";
+
+//console.log(a == b);//true
+console.log(a === b); //일치연산자 타입을 비교할 수 있음 */
+
+const age = 19;
+
+if(age > 19){
+    console.log('환영합니다.');
+} else if(age === 19){
+    console.log('수능 잘치세요!');
+} else {
+    console.log('안녕히가세요.')
+}
