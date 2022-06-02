6596f2e814296df0f10c56ee30d884fff4e573d4
김채은 <101077376+chaeeunK@users.noreply.github.com>
2022-06-02 09:57:53 +0900

    switch문
    switch문

---
+++ beginner-8(switch문).js
@@ -0,0 +1,27 @@
+// 사과 : 100원
+// 바나나 : 200원
+// 키위 : 300원
+// 멜론 : 500원
+// 수박 :500원
+// 사고 싶은 과일을 물어보고 가격 알려주기
+
+let fruit = prompt('무슨 과일을 사고싶나요?');
+
+switch(fruit){
+    case'사과' : 
+        console.log('100원입니다');
+        break;
+    case'바나나' : 
+        console.log('200원입니다');
+        break;
+    case'키위' : 
+        console.log('300원입니다');
+        break;
+    case'멜론' :
+    case'수박' : 
+        console.log('500원입니다');
+        break;
+    // else
+    default : 
+        console.log('그런 과일은 없습니다.')
+}
