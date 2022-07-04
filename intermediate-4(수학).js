ca63750e8721d2d2508abc05fbbb5a2508a237a9
김채은 <101077376+chaeeunK@users.noreply.github.com>
2022-07-04 09:27:25 +0900

    수학
    수학

---
+++ intermediate-4(수학).js
@@ -0,0 +1,24 @@
+/*
+Math.PI; -> 원주율
+Math.ceil() : 올림
+Math.floor() : 내림
+Math.round() : 반올림
+toFixed() : 소수점 자릿수
+isNaN() : NaN인지 아닌지 확인
+
+parselnt() : 문자열을 숫자로
+ex) let margin = '10px';
+parseInt(margin); // 10
+Number 랑 다름
+
+parseFloat() : 부동 소수점 반환
+
+Math.random() : 0 ~ 1 사이 무작위 숫자 생성
+1~100 사이 임의의 숫자를 뽑고 싶으면?
+Math.floor(Math.random()*100)+1 
+Math.mat() : 괄호 안에 있는 최대값
+Math.min() : 괄호 안에 있는 최소값
+Math.ads() : 절대값
+Math.pow(n,m) : 제곱
+Math.sqrt() : 제곱근
+*/
